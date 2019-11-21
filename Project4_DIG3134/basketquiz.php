7bab7ad26fd77140021e8186056d931c20b000ae
baseball19k <jimmyclement10@gmail.com>
2019-11-21 12:20:03 -0500

    developing the quiz

---
+++ Project4_DIG3134/basketquiz.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="basketquiz_result.php" method="post" id="basketquiz">
+
+      <!--question1-->
+      <div class="quizImage">
+      </div>
+      <div class="question">
+        <h2>What sport is this for?</h2>
+      </div>
+        <div class="answerleft">
+          <input type="radio" name="which_sport" value="football"><h4>Football</h4>
+        </div>
+        <div class="answerright">
+          <input type="radio" name="which_sport" value="cricket"><h4>Cricket</h4>
+        </div>
+        <div class="answerleft">
+          <input type="radio" name="which_sport" value="soccer"><h4>Soccer</h4>
+        </div>
+        <div class="answerright">
+          <input type="radio" name="which_sport" value="basketball"><h4>Basketball</h4>
+        </div>
+
+        <!--question2-->
+      <div class="quizImage">
+      </div>
+      <div class="question">
+        <h2>Who is this?</h2>
+      </div>
+      <div class="answerleft">
+        <input type="radio" name="mj" value="brady"><h4>Tom Brady</h4>
+      </div>
+      <div class="answerright">
+        <input type="radio" name="mj" value="james"><h4>Lebron James</h4>
+      </div>
+      <div class="answerleft">
+        <input type="radio" name="mj" value="jordan"><h4>Michael Jordan</h4>
+      </div>
+      <div class="answerright">
+        <input type="radio" name="mj" value="jeter"><h4>Derek Jeter</h4>
+      </div>
+
+        <!--question3-->
+        <div class="quizImage">
+        </div>
+    <div class="question">
+      <p>If you make a shot from here how many points do you get?</p>
+    </div>
+      <div class="answerleft">
+        <input type="radio" name="points" value="3"><h4>3</h4>
+      </div>
+      <div class="answerright">
+        <input type="radio" name="points" value="2"><h4>2</h4>
+      </div>
+      <div class="answerleft">
+        <input type="radio" name="points" value="5"><h4>5</h4>
+      </div>
+      <div class="answerright">
+        <input type="radio" name="points" value="1"><h4>1</h4>
+      </div>
+
+        <!--question4-->
+        <div class="quizImage">
+        </div>
+
+      <p>What sport is this for?</p>
+        <input type="radio" name="which_sport" value="football">Football<br>
+        <input type="radio" name="which_sport" value="cricket">Cricket<br>
+        <input type="radio" name="which_sport" value="soccer">Soccer<br>
+        <input type="radio" name="which_sport" value="basketball">Basketball<br>
+
+        <!--question5-->
+        <div class="quizImage">
+        </div>
+      <p>What sport is this for?</p>
+        <input type="radio" name="which_sport" value="football">Football<br>
+        <input type="radio" name="which_sport" value="cricket">Cricket<br>
+        <input type="radio" name="which_sport" value="soccer">Soccer<br>
+        <input type="radio" name="which_sport" value="basketball">Basketball<br>
+
+        <!--question6-->
+        <div class="quizImage">
+        </div>
+          <p>What sport is this for?</p>
+            <input type="radio" name="which_sport" value="football">Football<br>
+            <input type="radio" name="which_sport" value="cricket">Cricket<br>
+            <input type="radio" name="which_sport" value="soccer">Soccer<br>
+            <input type="radio" name="which_sport" value="basketball">Basketball<br>
+
+            <!--question7-->
+            <div class="quizImage">
+            </div>
+            <p>What sport is this for?</p>
+              <input type="radio" name="which_sport" value="football">Football<br>
+              <input type="radio" name="which_sport" value="cricket">Cricket<br>
+              <input type="radio" name="which_sport" value="soccer">Soccer<br>
+              <input type="radio" name="which_sport" value="basketball">Basketball<br>
+
+              <!--question8-->
+              <div class="quizImage">
+              </div>
+              <p>What sport is this for?</p>
+                <input type="radio" name="which_sport" value="football">Football<br>
+                <input type="radio" name="which_sport" value="cricket">Cricket<br>
+                <input type="radio" name="which_sport" value="soccer">Soccer<br>
+                <input type="radio" name="which_sport" value="basketball">Basketball<br>
+    </form>
+  </body>
+</html>
